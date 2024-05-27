3ef2fa4c38ea492af430f7bc3fbe970d6f726a5e
Ubuntu <ubuntu@ip-172-31-36-178.us-west-2.compute.internal>
2024-05-27 14:51:16 +0000

    fix vs platform build

--- platform/vs/docker-gbsyncd-vs.mk
+++ platform/vs/docker-gbsyncd-vs.mk
@@ -1,11 +1,7 @@
 # docker image for vs gbsyncd
 
 DOCKER_GBSYNCD_PLATFORM_CODE = vs
-ifeq ($(BLDENV),jammy)
 include $(PLATFORM_PATH)/../template/docker-gbsyncd-jammy.mk
-else
-include $(PLATFORM_PATH)/../template/docker-gbsyncd-bookworm.mk
-endif
 
 $(DOCKER_GBSYNCD_BASE)_DEPENDS += $(SYNCD_VS)
 
