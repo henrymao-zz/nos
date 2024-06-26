24bfd7627489e1b9f72d8486be6f36088dfac4de
Ubuntu <ubuntu@ip-172-31-38-169.us-west-2.compute.internal>
2024-06-26 05:18:07 +0000

    fix:add script to build rocks

---
+++ build_rocks.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+rocklist=("dockers/docker-nat" \
+          "dockers/docker-mux" \
+	  "dockers/docker-database" \
+	  "dockers/docker-orchagent" \
+	  "dockers/docker-sonic-gnmi" \
+	  "dockers/docker-platform-monitor" \
+	  "dockers/docker-teamd" \
+	  "dockers/docker-dhcp-relay" \
+	  "dockers/docker-router-advertiser" \
+	  "dockers/docker-macsec" \
+	  "dockers/docker-fpm-frr" \
+	  "dockers/docker-sonic-mgmt-framework" \
+	  "dockers/docker-lldp" \
+	  "dockers/docker-eventd" \
+	  "dockers/docker-sflow" \
+	  "dockers/docker-snmp")
+
+for rockitem in "${rocklist[@]}"
+do
+    mkdir -p $rockitem/debs
+    mkdir -p $rockitem/files
+    mkdir -p $rockitem/python-wheels
+
+    cp -r target/debs/jammy/*  $rockitem/debs/
+    cp -r target/files/jammy/* $rockitem/files/
+    cp -r target/python-wheels/jammy/* $rockitem/python-wheels/
+
+    pushd $rockitem
+
+    rockname=$(basename $rockitem)
+    rockfullname="${rockname}_1.0.0_amd64.rock"
+    rockcraft pack -v
+    sudo skopeo  --insecure-policy copy oci-archive:$rockfullname docker-daemon:$rockname:latest
+
+    popd
+
+    pushd target
+    docker save $rockname:latest  | pigz -c  >${rockname}.gz
+    popd 
+
+    docker rmi -f $rockname:latest
+
+done
+
