03ee2e83b2213e4388738a4153f01ca54f33921d
Ubuntu <ubuntu@ip-172-31-11-102.us-west-2.compute.internal>
2024-08-02 06:25:19 +0000

    fix scapy

--- rules/scapy.mk
+++ rules/scapy.mk
@@ -1,7 +1,11 @@
 # scapy python3 wheel
 
+ifneq ($(BLDENV),noble)
+
 SCAPY = scapy-2.4.5-py2.py3-none-any.whl
 $(SCAPY)_SRC_PATH = $(SRC_PATH)/scapy
 $(SCAPY)_PYTHON_VERSION = 3
 $(SCAPY)_TEST = n
 SONIC_PYTHON_WHEELS += $(SCAPY)
+
+endif
