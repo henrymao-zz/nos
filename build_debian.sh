18aee8920078291eb578e1da026fbc3cb2f13411
henrymao <henry.mao@canonical.com>
2025-07-07 08:05:16 +0000

    fix image build

--- build_debian.sh
+++ build_debian.sh
@@ -605,13 +605,13 @@ sudo LANG=c chroot $FILESYSTEM_ROOT chown root:shadow /etc/sonic/default_users.j
 sudo cp ./files/scripts/core_cleanup.py $FILESYSTEM_ROOT/usr/bin/core_cleanup.py
 
 ## Copy ASIC config checksum
-sudo chmod 755 files/build_scripts/generate_asic_config_checksum.py
-./files/build_scripts/generate_asic_config_checksum.py
-if [[ ! -f './asic_config_checksum' ]]; then
-    echo 'asic_config_checksum not found'
-    exit 1
-fi
-sudo cp ./asic_config_checksum $FILESYSTEM_ROOT/etc/sonic/asic_config_checksum
+#sudo chmod 755 files/build_scripts/generate_asic_config_checksum.py
+#./files/build_scripts/generate_asic_config_checksum.py
+#if [[ ! -f './asic_config_checksum' ]]; then
+#    echo 'asic_config_checksum not found'
+#    exit 1
+#fi
+#sudo cp ./asic_config_checksum $FILESYSTEM_ROOT/etc/sonic/asic_config_checksum
 
 ## Check if not a last stage of RFS build
 fi
