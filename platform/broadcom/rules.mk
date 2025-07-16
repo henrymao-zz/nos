f10e4c9ea3106d06dd93bc8ac3ca4075e7c267f6
henrymao <henry.mao@canonical.com>
2025-07-16 03:10:13 +0000

    add dsserve

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -1,4 +1,5 @@
 include $(PLATFORM_PATH)/switchdev.mk
+include $(PLATFORM_PATH)/sswsyncd.mk
 #include $(PLATFORM_PATH)/sai.mk
 # TODO: re-enable once the following have been updated for bookworm:
 # * Nokia
